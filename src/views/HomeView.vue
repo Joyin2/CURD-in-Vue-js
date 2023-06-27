65560bac9cd111cc0401138092b4af6ee21bd34b
Joyin2 <joyinlaskar2000@gmail.com>
2023-06-27 10:36:51 +0530

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Hello guys welcome to flerid</h1>
+    <TheWelcome />
+  </main>
+</template>
